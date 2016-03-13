243af051930f5e6b9ff792876d8fe2dc8cc41099
Iain Butler <i@iainbutler.co.uk>
2016-03-13 04:11:27 +0000

    List of files completed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
 
 	#GET /
 	def index
-
+		@files = Dir.glob("**/pub/*")
+		puts @files
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
 
+  get "graph/:file"     =>  "graph#show"
+
 end
